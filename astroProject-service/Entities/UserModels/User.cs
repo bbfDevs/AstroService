5dfd7897fb9eb16a40686b49cbded6d4ad669e75
Berke Can Ünlü <berke.c.unlu@gmail.com>
2024-12-29 22:04:40 +0300

    user record info

--- astroProject-service/Entities/UserModels/User.cs
+++ astroProject-service/Entities/UserModels/User.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace astroProject_service.Entities.UserModels
 {
-    public class User
+    public class User : RecordInfo
     {
         [Key]
         public Guid Id { get; set; }
---
+++ astroProject-service/Migrations/20241229190427_User-record-info.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace astroProject_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class Userrecordinfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                table: "User",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "User",
+                type: "timestamp",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProcessId",
+                table: "User",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "ProcessId",
+                table: "User");
+        }
+    }
+}
